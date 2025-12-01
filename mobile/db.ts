f5cc832d89b66743fca2e4c0a48b0352b83f86dc
Filipe Silva <filipess671@gmail.com>
2025-11-30 21:55:22 -0300

    restante das configuracoes

---
+++ mobile/db.ts
@@ -0,0 +1,42 @@
+export let db = {
+    "check": [
+        {
+            "userId": "5",
+            "completedActivities": [
+                {
+                    date: "2024-09-01",
+                    ids: [32, 19]
+                },
+                {
+                    date: "2024-09-02",
+                    ids: [28, 32]
+                },
+                {
+                    date: "2024-09-05",
+                    ids: [28]
+                },
+                {
+                    date: "2024-09-06",
+                    ids: [28, 32, 19, 20]
+                },
+                {
+                    date: "2024-09-08",
+                    ids: [28]
+                },
+                {
+                    date: "2024-09-10",
+                    ids: [28, 32]
+                },
+            ]
+        },
+        {
+            "userId": "6",
+            "completedActivities": [
+                {
+                    date: "2024-09-10",
+                    ids: [30]
+                }
+            ]
+        }
+    ]
+};
---
+++ mobile/tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+
+module.exports = {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
